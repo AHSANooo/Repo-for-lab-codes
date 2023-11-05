c097b15131301babb13f9649097baee68bc405da
Muhammad AHSAN <ahsankhetran72@gmail.com>
2023-11-05 10:55:31 +0500

    Create Basic Working calculator.cpp

---
+++ Basic Working calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a=0,b=0;
+//cout<<"MUHAMMAD AHSAN "<<endl;
+cout<<"Enter first number:";
+cin>>a;
+cout<<"Enter second number:";
+cin>>b;
+ cout<<"Addition:"<<a+b<<endl;
+  cout<<"Subtraction:"<<a-b<<endl;
+   cout<<"Multiplication:"<<a*b<<endl;
+    cout<<"Division:"<<a/b<<endl;
+
+    return 0;
+}
